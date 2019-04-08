27975fc7abdeeb8d1b1a0d2aa92b891242335d9c
KimSungWoo <rlatjddn23@gmail.com>
2019-04-08 21:22:22 +0900

    2493_탑

--- boj/Main2493.java
+++ boj/Main2493.java
@@ -7,12 +7,14 @@ import java.util.*;
 
 public class Main2493 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
 		int[] arr = new int[N+1]; //0번방은 안씀
 		int[] stack = new int[N]; // 스택, 탑의 index저장
 		int top = -1;
+		//String s = ""; // 최종 출력할 문자열
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 1; i <= N;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
@@ -23,14 +25,14 @@ public class Main2493 {
 			}
 			
 			if(top==-1) {//스택이 비어있으면
-				System.out.print("0 ");
+				sb.append("0 ");
 			}else {
-				System.out.print(stack[top] + " ");
+				sb.append(stack[top] + " ");
 			}
 			//스택에 내 탑을 쌓기
 			stack[++top] = i;
 			
 		}
-		System.out.println();
+		System.out.println(sb.toString());
 	} 
 } 
