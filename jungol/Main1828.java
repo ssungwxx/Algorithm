7d9ca6af6e04890541facd0b5279d81ae1465d16
KimSungWoo <rlatjddn23@gmail.com>
2019-04-08 20:53:37 +0900

    1828_냉장고

---
+++ jungol/Main1828.java
@@ -0,0 +1,45 @@
+package jungol;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+ 
+public class Main1828 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+         
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] < o2[0] ? -1 : 1;
+            }
+        });
+         
+        int cnt =1;
+        int min = arr[0][0];
+        int max = arr[0][1];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i][0] <= max && arr[i][1] >= min) {
+                min = (min > arr[i][0]) ? min : arr[i][0];
+                max = (max < arr[i][1]) ? max : arr[i][1];
+            }else {
+                cnt++;
+                min = arr[i][0];
+                max = arr[i][1];
+            }
+        }
+         
+        System.out.println(cnt);
+         
+    }
+}
