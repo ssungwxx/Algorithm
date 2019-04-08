e6768e61d6dea488794f4e0ace54d925731b06f1
KimSungWoo <rlatjddn23@gmail.com>
2019-04-08 21:22:50 +0900

    11399_ATM

---
+++ boj/Main11399.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main11399 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		int sum = 0;
+		int temp = 0;
+		for(int i = 0; i < arr.length; i++) {
+			temp += arr[i];
+			sum += temp;
+		}
+		
+		System.out.println(sum);
+		
+		sc.close();
+	}
+}
