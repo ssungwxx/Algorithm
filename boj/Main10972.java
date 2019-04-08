da354a893011c272171fb76562d0d6d89b4c6ea5
KimSungWoo <rlatjddn23@gmail.com>
2019-04-08 21:18:37 +0900

    10972_다음 순열

---
+++ boj/Main10972.java
@@ -0,0 +1,64 @@
+package boj;
+
+import java.util.*;
+
+public class Main10972 {
+    public static void main(String[] args) throws Exception {
+        Scanner io = new Scanner(System.in);
+
+        int n = io.nextInt();
+
+        int array[] = new int[n];
+
+        if (n >= 1 && n <= 10000) {
+            for (int i = 0; i < n; i++) {
+                array[i] = io.nextInt();
+            }
+
+            if (next_permutation(array)) {
+                for (int i = 0; i < n; i++) {
+                    System.out.print(array[i] + " ");
+                }
+            } else {
+                System.out.println(-1);
+            }
+        }
+
+
+        io.close();
+    }
+
+    public static boolean next_permutation(int[] a) {
+        int i = a.length - 1;
+        int j = a.length - 1;
+        int temp;
+
+        while (i > 0 && a[i - 1] >= a[i]) {
+            i-=1;
+        }
+
+        if (i <= 0) {
+            return false;
+        }
+
+        while (a[j] <= a[i - 1]) {
+            j-=1;
+        }
+
+        temp = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = temp;
+
+        j = a.length-1;
+
+        while (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i+=1;
+            j-=1;
+        }
+
+        return true;
+    }
+}
